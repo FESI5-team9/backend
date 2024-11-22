07f1d5c80883644deb2b6bf8f4d456ec4739ba7b
ckaanf <skywlstn777@gmail.com>
2024-11-22 23:07:44 +0900

    chore : project init

---
+++ mukitlist/src/main/java/com/fesi/mukitlist/MukitlistApplication.java
@@ -0,0 +1,13 @@
+package com.fesi.mukitlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukitlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukitlistApplication.class, args);
+	}
+
+}
