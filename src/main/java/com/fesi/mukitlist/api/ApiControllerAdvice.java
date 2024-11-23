6b2b50f58ec50be9e6ae669bc5a44a5bab5b4ace
ckaanf <skywlstn777@gmail.com>
2024-11-23 09:46:42 +0900

    feature : #1 - 글로벌 에러, response 생성

---
+++ src/main/java/com/fesi/mukitlist/api/ApiControllerAdvice.java
@@ -0,0 +1,23 @@
+package com.fesi.mukitlist.api;
+
+import java.net.BindException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.fesi.mukitlist.api.service.ApiResponse;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(BindException.class)
+	public ApiResponse<Object> bindException(BindException e) {
+		return ApiResponse.of(
+			HttpStatus.NOT_FOUND,
+			e.getMessage()
+		);
+	}
+}
