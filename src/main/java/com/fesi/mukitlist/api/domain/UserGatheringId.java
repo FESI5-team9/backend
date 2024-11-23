95055e0fec9f8b6d548e2ae173fb79217d0a1fba
ckaanf <skywlstn777@gmail.com>
2024-11-23 09:28:14 +0900

    feature : #1 - 모임 관련 도메인 생성

---
+++ src/main/java/com/fesi/mukitlist/api/domain/UserGatheringId.java
@@ -0,0 +1,23 @@
+package com.fesi.mukitlist.api.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class UserGatheringId {
+	private Long userId;
+	private Long gatheringId;
+
+	private UserGatheringId(Long userId, Long gatheringId) {
+		this.userId = userId;
+		this.gatheringId = gatheringId;
+	}
+
+	public static UserGatheringId of(Long userId, Long gatheringId) {
+		return new UserGatheringId(userId, gatheringId);
+	}
+}
