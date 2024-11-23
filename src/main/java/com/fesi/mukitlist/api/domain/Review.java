1150c955606a30c34fadb09841293f897db0bd2b
ckaanf <skywlstn777@gmail.com>
2024-11-23 09:28:49 +0900

    feature : #1 - 리뷰 도메인 생성

---
+++ src/main/java/com/fesi/mukitlist/api/domain/Review.java
@@ -0,0 +1,37 @@
+package com.fesi.mukitlist.api.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private int score;
+	private String comment;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@ManyToOne
+	private Gathering gathering;
+
+	@ManyToOne
+	private User user;
+
+}
