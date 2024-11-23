68d94d371f9a8011cb0160af976c0ac2df6974b6
ckaanf <skywlstn777@gmail.com>
2024-11-23 09:47:31 +0900

    feature : #1 - 사용자 도메인 생성

--- src/main/java/com/fesi/mukitlist/api/domain/User.java
+++ src/main/java/com/fesi/mukitlist/api/domain/User.java
@@ -1,4 +1,49 @@
 package com.fesi.mukitlist.api.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String companyName;
+
+
+	private String image;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	private LocalDateTime deletedAt;
+
 }
