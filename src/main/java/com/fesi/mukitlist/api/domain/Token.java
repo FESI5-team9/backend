2faa5a10e69f3686cbf868e22ee9718f8d3141d7
KorYejinLee <koryjlee@gmail.com>
2024-11-25 18:29:21 +0900

    #4 - feat: Token 도메인 생성

---
+++ src/main/java/com/fesi/mukitlist/api/domain/Token.java
@@ -0,0 +1,25 @@
+package com.fesi.mukitlist.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
+}
