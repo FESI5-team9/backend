3a487817ad724eeef02279905126e2008d71b2b3
KorYejinLee <koryjlee@gmail.com>
2024-11-30 18:47:17 +0900

    test: SpringBootTest annotation 사용 가능 여부 체크 해야함

--- src/test/java/com/fesi/mukitlist/api/repository/user/UserRegistrationTests.java
+++ src/test/java/com/fesi/mukitlist/api/repository/user/UserRegistrationTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserRegistrationTests {
