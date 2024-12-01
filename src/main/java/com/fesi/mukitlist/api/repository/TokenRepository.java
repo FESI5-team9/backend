83bd3d5b21fe73d0861c75cea7e89dfac09c788e
KorYejinLee <koryjlee@gmail.com>
2024-12-01 14:12:53 +0900

    Remove : TokenRepository no usage 삭제

--- src/main/java/com/fesi/mukitlist/api/repository/TokenRepository.java
+++ src/main/java/com/fesi/mukitlist/api/repository/TokenRepository.java
@@ -1,10 +1,8 @@
 package com.fesi.mukitlist.api.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import com.fesi.mukitlist.domain.auth.Token;
-import com.fesi.mukitlist.domain.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,9 +13,6 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
       on t.user.id = u.id\s
       where u.id = :id and (t.expired = false)\s
       """)
-    List<Token> findAllValidTokenByUser(User user);
     Optional<Token> findByToken(String token);
-    Optional<Token> findByUser(User user);
-    Optional<Token> findByUserAndExpiredFalse(User user);
 
 }
