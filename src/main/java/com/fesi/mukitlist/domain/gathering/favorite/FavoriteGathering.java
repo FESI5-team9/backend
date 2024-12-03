1ac3f19888cd741f576c629239fe2a86e79285e2
ckaanf <skywlstn777@gmail.com>
2024-12-03 11:30:10 +0900

    feature : #27 - 찜하기 모델 생성

---
+++ src/main/java/com/fesi/mukitlist/domain/gathering/favorite/FavoriteGathering.java
@@ -0,0 +1,24 @@
+package com.fesi.mukitlist.domain.gathering.favorite;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class FavoriteGathering {
+
+	@EmbeddedId
+	private FavoriteGatheringId id;
+
+	private FavoriteGathering(FavoriteGatheringId id) {
+		this.id = id;
+	}
+
+	public static FavoriteGathering of (FavoriteGatheringId id) {
+		return new FavoriteGathering(id);
+	}
+}
---
+++ src/main/java/com/fesi/mukitlist/domain/gathering/favorite/FavoriteGatheringId.java
@@ -0,0 +1,25 @@
+package com.fesi.mukitlist.domain.gathering.favorite;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class FavoriteGatheringId {
+
+	private Long userId;
+
+	private Long gatheringId;
+
+	public FavoriteGatheringId(Long userId, Long gatheringId) {
+		this.userId = userId;
+		this.gatheringId = gatheringId;
+	}
+
+	public static FavoriteGatheringId of(Long userId, Long gatheringId) {
+		return new FavoriteGatheringId(userId, gatheringId);
+	}
+}
