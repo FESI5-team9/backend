a4a4dd2fb58abb6453ea0e5cb0ecd4ef16e8deeb
ckaanf <skywlstn777@gmail.com>
2024-12-17 16:36:24 +0900

    fix : 모임 데이터 조회 테스트 작성

---
+++ src/test/java/com/fesi/mukitlist/domain/service/gathering/ParticipationServiceTest.java
@@ -0,0 +1,48 @@
+package com.fesi.mukitlist.domain.service.gathering;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ParticipationServiceTest {
+
+	@Autowired
+	private ParticipationService participationService;
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void joinGathering() {
+	}
+
+	@Test
+	void leaveGathering() {
+	}
+
+	@Test
+	void getParticipantsBy() {
+	}
+
+	@Test
+	void testGetParticipantsBy() {
+	}
+
+	@Test
+	void testGetParticipantsBy1() {
+	}
+
+	@Test
+	void getParticipantsWithFilters() {
+	}
+}
